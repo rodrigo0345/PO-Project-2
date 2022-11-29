67432164215b0ca35b0e630d18b15db703bbb046
Rodrigo Casanova <rodrigocasanova@pop-os.localdomain>
2022-11-29 19:42:16 +0000

    preparing to save Objects

--- src/Backend/Users/Musician.java
+++ src/Backend/Users/Musician.java
@@ -3,8 +3,11 @@ package Backend.Users;
 import java.util.Set;
 import java.util.TreeSet;
 
+import Backend.Instruments.Instrument;
+
 public class Musician extends User {
     private Set<Backend.Albums.Album> albums = new TreeSet<>();
+    private Set<Instrument> instruments = new TreeSet<>();
 
     public Musician(String name, String email, String username, String password) {
         super(name, email, username, password);
@@ -21,4 +24,17 @@ public class Musician extends User {
     public Set<Backend.Albums.Album> getAlbums() {
         return albums;
     }
+
+    public void addInstrument(Instrument instrument) {
+        instruments.add(instrument);
+    }
+
+    public void removeInstrument(Instrument instrument) {
+        instruments.remove(instrument);
+    }
+
+    public Set<Instrument> getInstruments() {
+        return instruments;
+    }
+    
 }
--- src/Frontend/Menus/Mmusico.java
+++ src/Frontend/Menus/Mmusico.java
@@ -1,6 +1,7 @@
 package Frontend.Menus;
 
 import java.util.Scanner;
+import java.util.Set;
 
 import Backend.Users.Musician;
 
@@ -33,18 +34,74 @@ public class Mmusico implements Menu {
     }
 
     public void executeOption() {
+        Scanner sc = new Scanner(System.in);
+        
         switch (option) {
             case 1:
-                System.out.println("Edit profile");
+                System.out.println("[1] - Edit name");
+                System.out.println("[2] - Edit username");
+                System.out.println("[3] - Edit email");
+                System.out.println("[4] - Edit password");
+                System.out.println("[5] - Edit email");
+
+                try {
+                    int option = sc.nextInt();
+                    switch (option) {
+                        case 1:
+                            System.out.println("New name: ");
+                            String name = sc.next();
+                            user.setName(name);
+                            break;
+                        case 2:
+                            System.out.println("New username: ");
+                            String surname = sc.next();
+                            user.setUsername(surname);
+                            break;
+                        case 3:
+                            System.out.println("New email: ");
+                            String email = sc.next();
+                            user.setEmail(email);
+                            break;
+                        case 4:
+                            System.out.println("New password: ");
+                            String password = sc.next();
+                            user.setPassword(password);
+                            break;
+                        case 5:
+                            System.out.println("New email: ");
+                            String email2 = sc.next();
+                            user.setEmail(email2);
+                            break;
+                        default:
+                            System.out.println("Invalid option");
+                            break;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid option");
+                }
                 break;
             case 2:
-                System.out.println("Associated albums");
+                Set<Backend.Albums.Album> albums = this.user.getAlbums();
+
+                for (Backend.Albums.Album album : albums) {
+                    if (album instanceof Backend.Albums.Album) {
+                        System.out.println(album.getTitulo());
+                    }
+                }
                 break;
             case 3:
-                System.out.println("Future recording sessions");
+                Set<Backend.Albums.Album> albums2 = this.user.getAlbums();
+
+                for (Backend.Albums.Album album : albums2) {
+                    if (album instanceof Backend.Albums.Album && !((Backend.Albums.AlbumEditado)album).isEdited()) {
+                        System.out.println(album);
+                    }
+                }
                 break;
             case 4:
-                System.out.println("Request an instrument for a specific recording session");
+                Set<Backend.Instruments.Instrument> availableInstruments = this.user.getInstruments();
+                
+                System.out.println("Instrument's name: ");
                 break;
             case 5:
                 System.out.println("See the state of all recording sessions");
